60e2abdf1974889b433a443147572a939b259378
AJ <aj.tauber98@gmail.com>
2019-07-11 10:04:19 +1000

    Working Version

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project 0: Tic Tac Toe
+
+This is a simple game I build
+
+## Features
+
+Nothing yet.
+
+## Bugs
+
+Entire Project.
+
+## TODO
+- network multiplayer
+- more colours
+- more intense emotions
+
+## Technologies
+- Made with jQuery
+- Also uses funText to make the text fun
--- js/main.js
+++ js/main.js
@@ -1,94 +1,156 @@
-console.log("Js Works");
+console.log("JS Works");
+var turn;
+var user;
+var row, col;
 
-// Win Outcomes
-let win00 = [0, 1, 2] // Top Across
-let win01 = [3, 4, 5] // Middle Across
-let win02 = [6, 7, 8] // Bottom Across
-let win03 = [0, 3, 6] // Left Down
-let win04 = [1, 4, 7] // Middle Down
-let win05 = [2, 5, 8] // Bottom Down
-let win06 = [0, 4, 8] // Left Diagonal Down
-let win07 = [2, 4, 6] // Right Diagonal Down
-
-// display the array
-// var ROWS = board.length;
-// var COLUMNS = board[0].length;
-
-
-
-//the 2d array that defines the board
-const board = [
-  [" ", " ", " "],
-  [" ", " ", " "],
-  [" ", " ", " "]
-];
 const game = {
-  player: "X",
-  isWinner: function(){
-    for (var i = 0; i < board.length; i++) {
-      for (var j = 0; j < board.length; j++) {
-        array[i]
-      }
-      board[i]++;
+  moveCounter: 0,
+  currentPlayer: "X",
 
-    },
+board:  [ null, null, null, null, null, null, null, null, null],
 
-    // if(player === win00 || player === win01 || player === win02 || player === win03 || player === win04 || player === win05 || player === win06 || player === win07)
-  }
+  checkWin: function(){
 
-}
-
-function checkBoard() {
-  for(var i = 0; i < board.length; i++) {
-    for(var j = 0; j < board[i].length; j++)
-    if(board[i][j] == "") {
-      return true;
+    if(game.moveCounter < 4) {
+      return false;
     }
-  }
-  return false;
-}
+
+    var board = game.board;
 
 
+    if (board[0] === board[1] && board[0] === board[2] && board[1]!== null) {
+      return true;
+    } if (board[0] === board[3] && board[0] === board[6] && board[0] !== null){
+      return true;
+    } if (board[1] === board[4] && board[1] === board[7] && board[1] !== null){
+      return true;
+    } if (board[3] === board[4] && board[3] === board[5] && board[3] !== null){
+      return true;
+    } if (board[2] === board[5] && board[2] === board[8] && board[2] !== null){
+      return true;
+    } if (board[6] === board[7] && board[6] === board[8] && board[6] !== null){
+      return true;
+    } if (board[0] === board[4] && board[0] === board[8] && board[0] !== null){
+      return true;
+    } if (board[2] === board[4] && board[2] === board[6] && board[2] !== null){
+      return true;
+    } else {
+      return false;
+    }
+
+},
+
+  winStatus: function (){
+    if (this.win === this.checkWin ){
+      console.log('have a winner');
+    }
+  },
+};
 
 
+//DOM MANIUPULATION SECTION
+
+$(document).ready(function(){
+
+  // GAME TRACKING
+  $('.button').click(function(){
+    var row = $(this).index();
+    var id = this.id;
+    var boardIndex = parseInt( this.id );
+
+  //START GAME
+
+    //check board for X or O
+    if (game.board[ boardIndex ]) {
+      console.log("pick another option 1");
+      return;
+    }
+
+      //allow click
+        $(this).html(game.currentPlayer);
 
 
+        //add the functions for the board back in
+          game.board[ boardIndex ] = game.currentPlayer;
+            console.log( " " + this.id);
+          game.moveCounter += 1;
+
+          // check for winner/ draw. produce signal if need be.
+          if( game.checkWin() ){
+            if (game.currentPlayer === "X"){
+              console.log('win for ' + game.currentPlayer);
+              return;
+            }
+            if (game.currentPlayer === "O"){
+              console.log('win for ' + game.currentPlayer);
+              return;
+            }
+          } else if(game.moveCounter === 9){
+            console.log('draw!');
+            return;
+          }
 
 
+  // flip the players back and forth
+    if (game.currentPlayer === 'X' ) {
+          game.currentPlayer = 'O';
+          return;
+    } if(game.currentPlayer === 'O'){
+          game.currentPlayer = 'X';
+          return;
+      }
+  });
+
+}); // $(document).ready()
 
 
-
-
-
-//get a reference to the stage
-// var stage = document.querySelector("#stage");
-
-
+// // checking for winning combinations
+// const checkWinners = function() {
+//   console.log(board[1]);
+//   //checking rows
+//   for (let i = 0; i < board.length; i++) {
 //
-// for(var row = 0; row < ROWS; row++)
-// {
-//     for(var column = 0; column < COLUMNS; column++)
-//     {
-//         //create a div HTML element called cell
-//         var cell = document.createElement("div");
-//
-//         //set its CSS class to cell
-//         cell.setAttribute("class", "cell");
-//
-//         //add the div HTML element to the stage
-//         stage.appendChild(cell);
-//
-//         //position the cell
-//         cell.style.top = row * (SIZE + SPACE) + "px";
-//         cell.style.left = column * (SIZE + SPACE) + "px";
-//         cell.addEventListener("click", clickHandler, false);
+//     if(board[i][0] != "" && board[i][0] == board[i][1] && board[i][1] === board[i][2] ) {
+//       console.log("rows: " +board[i][0] + "" +board[i][1]+""+board[i][2]);
+//       return true;
 //     }
+//   }
+//     //checking columns
+//     for(let j= 0; j < board.length; j++) {
+//       if(board[0][j] != "" && board[0][j] === board[1][j] && board[1][j] == board[2][j]) {
+//         console.log("column: "+board[0][j]+""+board[1][j]+""+board[2][j]);
+//         return true;
+//       }
+//     }
+//     //checking across
+//     if( board[0][0] != "" && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+//
+//         console.log("across1");
+//
+//         return true;
+//
+//     }
+//     if(board[0][2] != "" && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+//
+//       console.log("across2");
+//
+//         return true;
+//     }
+//     //if reached here, no winners
+//     return false;
 // }
 //
-//
-// function clickHandler(event)
-// {
-//     console.log(event);
-//     event.currentTarget.style.backgroundColor = "red";
-//     console.log("worked");
+// /* checking if there's more room to play, if not
+//    then it's a draw'*/
+// const checkBoard = function() {
+//     for(var i = 0; i < board.length; i++) {
+//       for(var j = 0; j < board[i].length; j++)
+//       if(board[i][j] == "") {
+//         return true;
+//       }
+//     }
+//     return false;
 // }
+//
+// Debuging
+console.log("YEW");
